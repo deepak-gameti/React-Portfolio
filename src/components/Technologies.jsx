77997e2753193d613af714ba2988f50deebfc132
deepak-gameti <159270881+deepak-gameti@users.noreply.github.com>
2024-05-11 23:27:54 +0530

    this is first commit

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,97 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { FaBootstrap } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { TbBrandJavascript } from "react-icons/tb";
+import { TbBrandHtml5 } from "react-icons/tb";
+import { TbBrandCss3 } from "react-icons/tb";
+import { animate, motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5, delay: 0 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5, delay: 0 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandHtml5 className="text-7xl text-orange-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandCss3 className="text-7xl text-sky-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandJavascript className="text-7xl text-yellow-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaBootstrap className="text-7xl text-purple-500" />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiTailwindCssFill className="text-7xl text-cyan-400" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technologies;
